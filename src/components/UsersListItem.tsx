724f4baa9de8deb51e76d555e934450707ef4fbb
Udean777 <uaart123@gmail.com>
2024-05-31 20:01:12 +0700

    feat: adding chat functionality with other users

--- src/components/UsersListItem.tsx
+++ src/components/UsersListItem.tsx
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useChatContext } from 'stream-chat-expo'
+import { useAuth } from '../providers/AuthProvider'
+import { router } from 'expo-router'
 
 const UsersListItem = ({ user }: any) => {
+    const { client } = useChatContext()
+    const { user: me } = useAuth()
+
+    async function onPress() {
+        const channel = client.channel("messaging", {
+            members: [me.id, user.id]
+        })
+
+        await channel.watch()
+
+        router.replace(`/(home)/channel/${channel.cid}`)
+    }
+
     return (
-        <View style={{ padding: 10, backgroundColor: "#fff" }}>
+        <TouchableOpacity onPress={onPress} style={{ padding: 10, backgroundColor: "#fff" }}>
             <Text style={{ fontWeight: "bold" }}>{user.full_name}</Text>
-        </View>
+        </TouchableOpacity>
     )
 }
 
